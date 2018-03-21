5a41940a89e1934c5fec3302cd0c1615914d95b2
Ali Bayati <Bayati.ali@icloud.com>
2018-03-20 21:24:06 -0500

    Initial Commit

---
+++ gitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gitExampleXC9
+//
+//  Created by Ali Bayati on 3/20/18.
+//  Copyright © 2018 Ali Bayati. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gitExampleXC9
+//
+//  Created by Ali Bayati on 3/20/18.
+//  Copyright © 2018 Ali Bayati. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
